3e8364b126a86622b3acc8553a617a0f0177f17b
훈성 <phunseong@gmail.com>
2021-12-10 20:41:07 +0900

    [feat] google login

--- app/src/main/java/com/hunseong/delivery/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hunseong/delivery/ui/home/HomeFragment.kt
@@ -1,12 +1,21 @@
 package com.hunseong.delivery.ui.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.hunseong.delivery.R
 import com.hunseong.delivery.databinding.FragmentHomeBinding
+import com.hunseong.delivery.ui.MainActivity
+import timber.log.Timber
+import java.lang.Exception
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
@@ -18,10 +27,34 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        binding.button.setOnClickListener {
-            val directions = HomeFragmentDirections.homeFragmentToDetailFragment()
-            findNavController().navigate(directions)
-        }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.firebase_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient((this.activity as MainActivity), gso)
+
+        binding.googleBtn.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            startActivityForResult(signInIntent, 100)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 100) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.result
+                Timber.tag("GoogleAccount").d("id : " + account.id + " / email : " + account.email + " / name : " + account.displayName)
+            } catch (e : Exception) {
+                Timber.tag("GoogleAccount").d("error : $e")
+            }
+        }
     }
 }
